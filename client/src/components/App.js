d336e4a00d73c85555b930a401dd9e4a94174f04
InJoon-L <dlswns619@naver.com>
2021-03-21 23:03:04 +0900

    make movieInfo page and fix props error

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,6 +10,7 @@ import LandingPage from './views/LandingPage/LandingPage'
 import LoginPage from './views/LoginPage/LoginPage';
 import RegisterPage from './views/RegisterPage/RegisterPage';
 import Auth from '../hoc/auth'
+import MovieDetail from './views/MovieDetail/MovieDetail';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null )  } />
           <Route exact path="/login" component={Auth(LoginPage, false) } />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/movie/:movieId" component={Auth(MovieDetail, null)} />
         </Switch>
       </div>
     </Router>
